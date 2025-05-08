793cef87544b0eae1d5ffa6f9789582e4fb36e48
shokhruzbek <ashokhruzbek@gmail.com>
2025-05-08 18:26:30 +0500

    i added browser router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home";
 import MyPosts from "./pages/MyPosts";
@@ -12,14 +12,17 @@ import Signup from "./components/Signup";
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/my-posts" element={<MyPosts />} />
-        <Route path="/my-likes" element={<MyLikes />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/my-posts" element={<MyPosts />} />
+          <Route path="/my-likes" element={<MyLikes />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
